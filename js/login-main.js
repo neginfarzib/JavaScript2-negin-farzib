daeabdb8cb7b8e0d5db7bf3d8e60f18f811a453a
neginfarzib <88443139+neginfarzib@users.noreply.github.com>
2025-11-07 23:10:30 +0100

    login page is done

--- js/login-main.js
+++ js/login-main.js
@@ -1,9 +1,10 @@
 import { loginUser } from './auth.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const form = document.getElementById('login-form');
 form.addEventListener('submit', (e) => {
-        e.preventDefault();
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value;
-    loginUser(email, password);
+  e.preventDefault();
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value;
+  loginUser(email, password);
 });
