a0a6736dd13664f09a3d09c8b853fe482f277d6c
neginfarzib <88443139+neginfarzib@users.noreply.github.com>
2025-11-09 01:16:44 +0100

    register page is done. index.html for all blog post is done

--- js/blog-feed-main.js
+++ js/blog-feed-main.js
@@ -1,129 +1,151 @@
-import {allPosts, searchPostAPI} from "./manage-all-post.js";
-const base_url = "https://v2.api.noroff.dev";
-const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric' , minute: 'numeric', hour12: false};
+import { allPosts, searchPostAPI } from './manage-all-post.js';
+const base_url = 'https://v2.api.noroff.dev';
+const options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: false,
+};
 
 /**
  * Fetching all posts. Then sort them by date
  *
  * @return {Promise<object[]>} A promise that resolves to an array of post objects sorted by date.
-* */
-export async function dateSortedAllPosts(){
-    let posts = await allPosts();
-    let sortedAllPosts = posts.data.sort((a, b) => new Date(b.created) - new Date(a.created));
-    return sortedAllPosts;
+ * */
+export async function dateSortedAllPosts() {
+  let posts = await allPosts();
+  let sortedAllPosts = posts.data.sort(
+    (a, b) => new Date(b.created) - new Date(a.created)
+  );
+  return sortedAllPosts;
 }
 
-document.addEventListener('DOMContentLoaded',async () => {
-    const blogPostsThumbnail = document.getElementById('blog-post-container');
-    const errorMessageElement = document.getElementById('errorMessage');
+document.addEventListener('DOMContentLoaded', async () => {
+  const blogPostsThumbnail = document.getElementById('blog-post-container');
+  const errorMessageElement = document.getElementById('errorMessage');
 
-    const posts = await dateSortedAllPosts();
-    console.log(posts.length)
-    displayPosts(posts);
+  const posts = await dateSortedAllPosts();
+  console.log(posts.length);
+  displayPosts(posts);
 
-    function displayPosts(posts){
-        blogPostsThumbnail.innerHTML = '';
+  function displayPosts(posts) {
+    blogPostsThumbnail.innerHTML = '';
 
-        const inputSearchPostsDiv = document.createElement('div');
-        inputSearchPostsDiv.classList.add('search-blog-thumbnail');
+    const inputSearchPostsDiv = document.createElement('div');
+    inputSearchPostsDiv.classList.add('w-75', 'mx-auto', 'mb-4', 'my-4');
 
-        const inputBtnSearchPostsDiv = document.createElement('div');
-        inputBtnSearchPostsDiv.classList.add('search-blog-thumbnail-input-btn');
-        const inputSearchPosts = document.createElement('input');
-        inputSearchPosts.type = 'text';
-        inputSearchPosts.id = 'searchPosts';
-        inputSearchPosts.placeholder = 'Search all posts ...'
-        inputBtnSearchPostsDiv.appendChild(inputSearchPosts);
-        const searchPostsBtn = document.createElement('button');
-        searchPostsBtn.classList.add('search-blog-thumbnail-btn')
-        searchPostsBtn.textContent = 'Search';
-        searchPostsBtn.addEventListener('click', async function (e) {
-            e.preventDefault();
-            const searchInput = document.getElementById('searchPosts').value.trim()
-            const searchPostsList = await searchPostAPI(searchInput);
-            displayPosts(searchPostsList);
-        })
-        inputBtnSearchPostsDiv.appendChild(searchPostsBtn);
-        inputSearchPostsDiv.appendChild(inputBtnSearchPostsDiv);
+    const inputBtnSearchPostsDiv = document.createElement('div');
+    inputBtnSearchPostsDiv.classList.add('d-flex');
+    const inputSearchPosts = document.createElement('input');
+    inputSearchPosts.type = 'text';
+    inputSearchPosts.id = 'searchPosts';
+    inputSearchPosts.placeholder = 'Search all posts ...';
+    inputSearchPosts.classList.add('form-control', 'me-2');
+    inputBtnSearchPostsDiv.appendChild(inputSearchPosts);
+    const searchPostsBtn = document.createElement('button');
+    searchPostsBtn.classList.add('btn', 'btn-primary', 'px-4');
+    searchPostsBtn.textContent = 'Search';
+    searchPostsBtn.addEventListener('click', async function (e) {
+      e.preventDefault();
+      const searchInput = document.getElementById('searchPosts').value.trim();
+      const searchPostsList = await searchPostAPI(searchInput);
+      displayPosts(searchPostsList);
+    });
+    inputBtnSearchPostsDiv.appendChild(searchPostsBtn);
+    inputSearchPostsDiv.appendChild(inputBtnSearchPostsDiv);
 
-        const editLineSeperator = document.createElement('div');
-        editLineSeperator.classList.add("edit-line-separator");
-        inputSearchPostsDiv.appendChild(editLineSeperator);
+    const editLineSeperator = document.createElement('div');
+    editLineSeperator.classList.add('mt-4', 'border-5');
+    inputSearchPostsDiv.appendChild(editLineSeperator);
 
-        blogPostsThumbnail.appendChild(inputSearchPostsDiv);
+    blogPostsThumbnail.appendChild(inputSearchPostsDiv);
 
+    const maxPosts = posts.length;
+    const postToShow = posts.slice(0, maxPosts);
 
-        
-        const maxPosts = posts.length;
-        const postToShow = posts.slice(0, maxPosts);
+    postToShow.forEach((post) => {
+      const blogThumbnail = document.createElement('div');
+      blogThumbnail.classList.add('card', 'w-75', 'mx-auto', 'shadow', 'mb-4');
 
-        postToShow.forEach(post =>{
-            const blogThumbnail = document.createElement('div');
-            blogThumbnail.classList.add('blog-thumbnail');
+      const blogThumbnailDiv = document.createElement('div');
+      blogThumbnailDiv.classList.add('card-body');
 
-            if(post.author){
-                const authorHref = document.createElement('a');
-                authorHref.href = 'post/user-posts.html?name-of-user=' + post.author.name;
+      if (post.author) {
+        const authorHref = document.createElement('a');
+        authorHref.classList.add(
+          'text-decoration-none',
+          'text-dark',
+          'd-flex',
+          'align-items-center',
+          'mb-2'
+        );
+        authorHref.href =
+          'post/user-posts.html?name-of-user=' + post.author.name;
 
-                const author = document.createElement('div');
+        const postAuthorIcon = document.createElement('img');
+        postAuthorIcon.src = 'assets/person-icon.svg';
+        postAuthorIcon.alt = 'author-icon';
+        postAuthorIcon.width = '32';
+        postAuthorIcon.height = '32';
+        postAuthorIcon.classList.add('me-2');
+        authorHref.appendChild(postAuthorIcon);
 
-                const postAuthorIcon = document.createElement('img');
-                postAuthorIcon.src = 'assets/person-icon.svg';
-                postAuthorIcon.alt = 'author-icon'
-                author.appendChild(postAuthorIcon);
+        const postAuthor = document.createElement('h4');
+        postAuthor.classList.add('card-title', 'mb-0');
+        postAuthor.textContent = post.author.name;
+        authorHref.appendChild(postAuthor);
 
-                const postAuthor = document.createElement('h4');
-                postAuthor.textContent = post.author.name;
-                author.appendChild(postAuthor)
+        blogThumbnailDiv.appendChild(authorHref);
+      }
 
-                authorHref.appendChild(author)
-                blogThumbnail.appendChild(authorHref);
-            }
+      const postCreatedTime = document.createElement('p');
+      postCreatedTime.classList.add('text-muted', 'small', 'mb-3');
+      const date = new Date(post.created);
+      postCreatedTime.textContent = date.toLocaleDateString('en-US', options);
+      blogThumbnailDiv.appendChild(postCreatedTime);
 
-            const postCreatedTime = document.createElement('p');
-            postCreatedTime.classList.add('blog-thumbnail-created-time-p')
-            const date = new Date(post.created)
-            postCreatedTime.textContent = date.toLocaleDateString('en-US', options);
-            blogThumbnail.appendChild(postCreatedTime);
+      const blogThumbnailHref = document.createElement('a');
+      blogThumbnailHref.classList.add('text-decoration-none', 'text-dark');
+      blogThumbnailHref.href = 'post/index.html?blog-post-id=' + post.id;
+      // blogThumbnailHref.target = '_blank';
 
-            const blogThumbnailHref = document.createElement('a');
-            blogThumbnailHref.href = 'post/index.html?blog-post-id='+post.id;
-            // blogThumbnailHref.target = '_blank';
+      const postTitle = document.createElement('h6');
+      postTitle.classList.add('fw-semibold');
+      postTitle.textContent = post.title;
+      blogThumbnailHref.appendChild(postTitle);
 
-            const postTitle = document.createElement('h6');
-            postTitle.textContent = post.title;
-            blogThumbnailHref.appendChild(postTitle)
-            blogThumbnail.appendChild(blogThumbnailHref);
+      if (post.media) {
+        const postImage = document.createElement('img');
+        postImage.classList.add('card-img-top', 'rounded', 'mb-2');
+        postImage.src = post.media?.url || '';
+        postImage.alt = post.media?.alt || '';
+        blogThumbnailHref.appendChild(postImage);
+      }
 
-            if(post.media){
-                const postImage = document.createElement('img');
-                postImage.classList.add('blog-thumbnail-img')
-                postImage.src = post.media?.url || '';
-                postImage.alt = post.media?.alt || '';
-                blogThumbnailHref.appendChild(postImage)
-                blogThumbnail.appendChild(blogThumbnailHref);
-            }
+      const postContent = document.createElement('p');
+      postContent.classList.add('card-text');
+      postContent.textContent =
+        post.body?.split(/\s+/).slice(0, 50).join(' ') || '';
+      blogThumbnailHref.appendChild(postContent);
 
-            const postContent = document.createElement('p');
-            postContent.textContent = post.body?.split(/\s+/).slice(0, 50).join(' ') || '';
-            blogThumbnailHref.appendChild(postContent);
-            blogThumbnail.appendChild(blogThumbnailHref);
+      const postReadMore = document.createElement('p');
+      postReadMore.classList.add('text-primary', 'fw-semibold');
+      postReadMore.textContent = 'Read more...';
+      blogThumbnailHref.appendChild(postReadMore);
+      blogThumbnailDiv.appendChild(blogThumbnailHref);
 
-            const postReadMore = document.createElement('p');
-            postReadMore.textContent = 'Read more...';
-            blogThumbnailHref.appendChild(postReadMore);
-            blogThumbnail.appendChild(blogThumbnailHref);
-
-
-            // Append the product box to the container
-            blogPostsThumbnail.appendChild(blogThumbnail);
-        });
-        const errorMessageDiv = document.createElement('div');
-        errorMessageDiv.classList.add('row');
-        const errorMessageP = document.createElement('p');
-        errorMessageP.id='errorMessage';
-        errorMessageDiv.appendChild(errorMessageP);
-        blogPostsThumbnail.appendChild(errorMessageDiv);
-
-    }
-})
+      blogThumbnail.appendChild(blogThumbnailDiv);
+      // Append the product box to the container
+      blogPostsThumbnail.appendChild(blogThumbnail);
+    });
+    const errorMessageDiv = document.createElement('div');
+    errorMessageDiv.classList.add('row');
+    const errorMessageP = document.createElement('p');
+    errorMessageP.classList.add('alert', 'alert-danger', 'mt-4', 'd-none');
+    errorMessageP.id = 'errorMessage';
+    errorMessageDiv.appendChild(errorMessageP);
+    blogPostsThumbnail.appendChild(errorMessageDiv);
+  }
+});
