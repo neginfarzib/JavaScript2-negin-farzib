2cab09fd15fb89233b6fec78c8e93e7c71dccae2
neginfarzib <88443139+neginfarzib@users.noreply.github.com>
2025-11-09 14:13:02 +0100

    manage all post converted

--- js/manage-all-post-main.js
+++ js/manage-all-post-main.js
@@ -1,95 +1,132 @@
-import {deleteBlogPost, allUsersPosts} from "./manage-all-post.js";
-import {checkIfAuthenticated} from "./auth-check.js";
+import { deleteBlogPost, allUsersPosts } from './manage-all-post.js';
+import { checkIfAuthenticated } from './auth-check.js';
 
 checkIfAuthenticated();
 
-document.addEventListener('DOMContentLoaded',async () => {
-    const editPostsThumbnail = document.getElementById('edit-posts-thumbnail');
+document.addEventListener('DOMContentLoaded', async () => {
+  const editPostsThumbnail = document.getElementById('edit-posts-thumbnail');
 
-    const nameUser =localStorage.getItem('name');
-    const posts = await allUsersPosts(nameUser);
-    displayPosts(posts.data);
+  const nameUser = localStorage.getItem('name');
+  const posts = await allUsersPosts(nameUser);
+  displayPosts(posts.data);
 
-    function displayPosts(posts){
-        editPostsThumbnail.innerHTML = '';
-        posts.forEach((post) => {
-            const blogThumbnail = document.createElement('div');
-            blogThumbnail.classList.add('blog-thumbnail');
+  function displayPosts(posts) {
+    editPostsThumbnail.innerHTML = '';
+    posts.forEach((post) => {
+      const blogThumbnail = document.createElement('div');
+      blogThumbnail.classList.add('card', 'w-75', 'mx-auto', 'shadow', 'mb-4');
 
-            const blogThumbnailHref = document.createElement('a');
-            blogThumbnailHref.href = '../post/index.html?blog-post-id='+post.id;
-            // blogThumbnailHref.target = '_blank';
+      const blogThumbnailDiv = document.createElement('div');
+      blogThumbnailDiv.classList.add('card-body');
 
-            const postTitle = document.createElement('h4');
-            postTitle.textContent = post.title;
-            blogThumbnailHref.appendChild(postTitle)
-            blogThumbnail.appendChild(blogThumbnailHref);
+      const blogThumbnailHref = document.createElement('a');
+      blogThumbnailHref.href = '../post/index.html?blog-post-id=' + post.id;
+      blogThumbnailHref.classList.add('text-decoration-none', 'text-dark');
+      // blogThumbnailHref.target = '_blank';
 
-            const postImage = document.createElement('img');
-            postImage.classList.add('blog-thumbnail-img')
-            postImage.src = post.media?.url || '';
-            postImage.alt = post.media?.alt || '';
-            blogThumbnailHref.appendChild(postImage)
-            blogThumbnail.appendChild(blogThumbnailHref);
+      const postTitle = document.createElement('h4');
+      postTitle.classList.add('card-title');
+      postTitle.textContent = post.title;
+      blogThumbnailHref.appendChild(postTitle);
 
-            const postContent = document.createElement('p');
-            postContent.textContent = post.body?.split(/\s+/).slice(0, 50).join(' ') || '';
-            blogThumbnailHref.appendChild(postContent);
-            blogThumbnail.appendChild(blogThumbnailHref);
+      const postImage = document.createElement('img');
+      postImage.classList.add('card-img-top', 'rounded', 'mb-2');
+      postImage.src = post.media?.url || '';
+      postImage.alt = post.media?.alt || '';
+      blogThumbnailHref.appendChild(postImage);
 
-            const postReadMore = document.createElement('p');
-            postReadMore.textContent = 'Read more...';
-            blogThumbnailHref.appendChild(postReadMore);
-            blogThumbnail.appendChild(blogThumbnailHref);
+      const postContent = document.createElement('p');
+      postContent.classList.add('card-text');
+      postContent.textContent =
+        post.body?.split(/\s+/).slice(0, 50).join(' ') || '';
+      blogThumbnailHref.appendChild(postContent);
 
-            /* banner */
-            const createPostThumbnailBannerDiv = document.createElement('div');
-            createPostThumbnailBannerDiv.classList.add('creat-post-thumbnail-banner')
+      const postReadMore = document.createElement('p');
+      postReadMore.classList.add('text-primary', 'fw-semibold');
+      postReadMore.textContent = 'Read more...';
+      blogThumbnailHref.appendChild(postReadMore);
 
-            const postBannerHrefEdit = document.createElement('a');
-            postBannerHrefEdit.href = '../post/edit.html?blog-post-id='+post.id;
-            const postImageBannerEdit = document.createElement('img');
-            postImageBannerEdit.classList.add('creat-post-thumbnail-banner-img')
-            postImageBannerEdit.src = '../assets/pen.svg';
-            postImageBannerEdit.alt = 'edit';
-            postBannerHrefEdit.appendChild(postImageBannerEdit);
-            createPostThumbnailBannerDiv.appendChild(postBannerHrefEdit);
+      blogThumbnailDiv.appendChild(blogThumbnailHref);
+      blogThumbnail.appendChild(blogThumbnailDiv);
 
-            const postBannerHrefView = document.createElement('a');
-            postBannerHrefView.href = '../post/index.html?blog-post-id='+post.id;
-            // postBannerHrefView.target = '_blank';
-            const postImageBannerView = document.createElement('img');
-            postImageBannerView.classList.add('creat-post-thumbnail-banner-img')
-            postImageBannerView.src = '../assets/eye.svg';
-            postImageBannerView.alt = 'view';
-            postBannerHrefView.appendChild(postImageBannerView);
-            createPostThumbnailBannerDiv.appendChild(postBannerHrefView);
+      /* banner */
+      const createPostThumbnailBannerDiv = document.createElement('div');
+      createPostThumbnailBannerDiv.classList.add(
+        'd-flex',
+        'justify-content-between',
+        'align-items-center',
+        'gap-3',
+        'p-2',
+        'rounded'
+      );
+      createPostThumbnailBannerDiv.style.backgroundColor = '#cc7eb5';
 
-            const postBannerHrefDelete = document.createElement('a');
-            postBannerHrefDelete.href = '#rr';
-            postBannerHrefDelete.onclick = function(event) {
-                event.preventDefault();
-                deleteBlogPost(post.id);
-            };
-            const postImageBannerDelete = document.createElement('img');
-            postImageBannerDelete.classList.add('creat-post-thumbnail-banner-img')
-            postImageBannerDelete.src = '../assets/bin.svg';
-            postImageBannerDelete.alt = 'delete';
-            postBannerHrefDelete.appendChild(postImageBannerDelete);
-            createPostThumbnailBannerDiv.appendChild(postBannerHrefDelete);
+      const postBannerHrefEdit = document.createElement('a');
+      postBannerHrefEdit.href = '../post/edit.html?blog-post-id=' + post.id;
+      postBannerHrefEdit.classList.add(
+        'btn',
+        'btn-outline-secondary',
+        'btn-sm',
+        'p-2'
+      );
+      const postImageBannerEdit = document.createElement('img');
+      postImageBannerEdit.classList.add('creat-post-thumbnail-banner-img');
+      postImageBannerEdit.src = '../assets/pen.svg';
+      postImageBannerEdit.alt = 'edit';
+      postImageBannerEdit.width = '20';
+      postImageBannerEdit.height = '20';
+      postBannerHrefEdit.appendChild(postImageBannerEdit);
+      createPostThumbnailBannerDiv.appendChild(postBannerHrefEdit);
 
-            blogThumbnail.appendChild(createPostThumbnailBannerDiv);
+      const postBannerHrefView = document.createElement('a');
+      postBannerHrefView.href = '../post/index.html?blog-post-id=' + post.id;
+      postBannerHrefView.classList.add(
+        'btn',
+        'btn-outline-secondary',
+        'btn-sm',
+        'p-2'
+      );
+      // postBannerHrefView.target = '_blank';
+      const postImageBannerView = document.createElement('img');
+      postImageBannerView.classList.add('creat-post-thumbnail-banner-img');
+      postImageBannerView.src = '../assets/eye.svg';
+      postImageBannerView.alt = 'view';
+      postImageBannerView.width = '20';
+      postImageBannerView.height = '20';
+      postBannerHrefView.appendChild(postImageBannerView);
+      createPostThumbnailBannerDiv.appendChild(postBannerHrefView);
 
+      const postBannerHrefDelete = document.createElement('a');
+      postBannerHrefDelete.classList.add(
+        'btn',
+        'btn-outline-secondary',
+        'btn-sm',
+        'p-2'
+      );
+      postBannerHrefDelete.href = '#rr';
+      postBannerHrefDelete.onclick = function (event) {
+        event.preventDefault();
+        deleteBlogPost(post.id);
+      };
+      const postImageBannerDelete = document.createElement('img');
+      postImageBannerDelete.classList.add('creat-post-thumbnail-banner-img');
+      postImageBannerDelete.src = '../assets/bin.svg';
+      postImageBannerDelete.alt = 'delete';
+      postImageBannerDelete.width = '20';
+      postImageBannerDelete.height = '20';
+      postBannerHrefDelete.appendChild(postImageBannerDelete);
+      createPostThumbnailBannerDiv.appendChild(postBannerHrefDelete);
 
-            // Append the product box to the container
-            editPostsThumbnail.appendChild(blogThumbnail);
-        });
-        const errorMessageDiv = document.createElement('div');
-        errorMessageDiv.classList.add('row');
-        const errorMessageP = document.createElement('p');
-        errorMessageP.id='errorMessage';
-        errorMessageDiv.appendChild(errorMessageP);
-        editPostsThumbnail.appendChild(errorMessageDiv);
+      blogThumbnail.appendChild(createPostThumbnailBannerDiv);
 
-    }
-})
+      // Append the product box to the container
+      editPostsThumbnail.appendChild(blogThumbnail);
+    });
+    const errorMessageDiv = document.createElement('div');
+    errorMessageDiv.classList.add('row');
+    const errorMessageP = document.createElement('p');
+    errorMessageP.id = 'errorMessage';
+    errorMessageDiv.appendChild(errorMessageP);
+    editPostsThumbnail.appendChild(errorMessageDiv);
+  }
+});
